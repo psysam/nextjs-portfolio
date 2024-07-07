b47b9488a6bc24915716e35d7a8d1b6fcba374b8
Prince Singh Yadav <89747571+psysam@users.noreply.github.com>
2024-07-08 02:38:36 +0545

    final commit

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-            <span className="text-white">Hello, I'm Prince & I'm </span>
+            <span className="text-white">Hello, I'm Prince &apos I'm </span>
             <br></br>
             <TypeAnimation className=""
               sequence={[
