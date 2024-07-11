16de51412332cb9f4fd38a961bf76df641c577fd
Prince Singh Yadav <89747571+psysam@users.noreply.github.com>
2024-07-12 01:32:43 +0545

    final commit

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -45,15 +45,15 @@ const HeroSection = () => {
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-t from-gray-50 to-purple-500  text-white hover:to-pink-300  hover:shadow-lg hover:shadow-gray-400 font-extrabold font-sans"
             >
-              Hire Me
+              Hire Me!
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-t from-gray-50 to-purple-500  text-white mt-3 hover:shadow-lg hover:shadow-gray-400 "
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-gradient-to-t from-gray-50 to-purple-500 rounded-full px-5 py-2  font-extrabold font-sans ">
                 Download CV
               </span>
             </Link>
